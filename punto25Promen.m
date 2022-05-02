95ecb0fd6f3b918355714992e1ed8af26d536dde
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2022-05-02 17:03:43 -0300

    Graficos de E(t) y S(t) con distintos multiplicadores

--- punto25Promen.m
+++ punto25Promen.m
@@ -4,8 +4,18 @@ function [Multiplicador R D] = punto25Promen(T,H,C,h0,finalLlenado,paso, Ma, Mb,
   D = [];
   index = 1;
   for m=Ma:Mp:Mb
-  [Taux Yaux] = punto21Promen(T,H,C,h0,finalLlenado,paso, m, 1);
+  [Taux Yaux] = punto21Promen(T,H,C,h0,finalLlenado,paso, m, 0);
   [Evalues, Svalues] = punto23APromen(Taux,Yaux,H,C, m);
+  figure
+  hold on
+   plot(Taux,Evalues,Taux,Svalues);
+   title(['Hidrograma de entrada y de salida (Multiplicador:' num2str(m) ')']);
+   ylabel('Flujo de entrada/salida (m^3/s)');
+   xlabel('Tiempo (s)');
+   legend({'Entrada','Salida'},'Location','northwest');
+   axis tight;
+   grid on;
+  hold off
   [r d] = punto23BPromen(Evalues, Svalues, paso);
   Multiplicador(index) = m;
   R(index) = r;
